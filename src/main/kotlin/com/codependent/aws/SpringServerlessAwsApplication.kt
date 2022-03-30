c7b118acc26086597e1e12c93cb8f38285fbafa6
jainigo <jainigo@profile.es>
2022-03-30 13:39:11 +0200

    functional bean registration

--- src/main/kotlin/com/codependent/aws/SpringServerlessAwsApplication.kt
+++ src/main/kotlin/com/codependent/aws/SpringServerlessAwsApplication.kt
@@ -2,31 +2,58 @@ package com.codependent.aws
 
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
+import org.springframework.cloud.function.context.FunctionRegistration
+import org.springframework.cloud.function.context.FunctionalSpringApplication
+import org.springframework.cloud.function.context.catalog.FunctionTypeUtils
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.support.GenericApplicationContext
 import java.security.SecureRandom
-import java.util.*
+import java.util.function.Function
+import java.util.function.Supplier
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+
 
 @SpringBootApplication
-class SpringServerlessAwsApplication{
+class SpringServerlessAwsApplication : ApplicationContextInitializer<GenericApplicationContext> {
 
-    private val random = SecureRandom()
+    private val secureRandom = SecureRandom()
     private val logger = LoggerFactory.getLogger(javaClass)
+    private val objectMapper = jacksonObjectMapper()
 
-    @Bean
-    fun uppercase(): (Map<String, String>) -> Map<String, String> = {
-        logger.info("uppercase() Received {}", it)
-        it.map { entry -> Pair(entry.key.uppercase(), entry.value.uppercase()) }.toMap()
-    }
+    override fun initialize(context: GenericApplicationContext) {
 
-    @Bean
-    fun random(): (Map<String, String>) -> Map<String, String> = {
-        logger.info("random() Received {}", it)
-        it.mapValues { entry -> "$entry${random.nextDouble()}" }
+        val uppercase = Function<Any, Map<String, String>> {
+            logger.info("uppercase() Received {}", it)
+            logger.info("type {}", it.javaClass)
+            val ba: ByteArray = if(it is String) {
+                it.toByteArray()
+            } else {
+                it as ByteArray
+            }
+            val map: Map<String, String> = objectMapper.readValue(ba)
+            val proc = map.map { entry -> Pair(entry.key.uppercase(), entry.value.uppercase()) }.toMap()
+            proc
+        }
+
+        val random= Function<Map<String, String>, Map<String, String>> {
+            logger.info("random() Received {}", it)
+            it.mapValues { entry -> "$entry${secureRandom.nextDouble()}" }
+        }
+
+        context.registerBean("uppercase", FunctionRegistration::class.java, Supplier {
+            FunctionRegistration(uppercase)
+                .type(FunctionTypeUtils.discoverFunctionType(uppercase, "uppercaseFn", context))
+        })
+        context.registerBean("random", FunctionRegistration::class.java, Supplier {
+            FunctionRegistration(random)
+                .type(FunctionTypeUtils.discoverFunctionType(random, "randomFn", context))
+        })
     }
 
 }
 
 fun main(args: Array<String>) {
-    runApplication<SpringServerlessAwsApplication>(*args)
+    FunctionalSpringApplication.run(SpringServerlessAwsApplication::class.java, *args)
+    //runApplication<SpringServerlessAwsApplication>(*args)
 }
